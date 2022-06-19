1358b55c76efa6a97db343823f4a9f31b6bfb9d0
Vini-Yking <mvdsfg@gmail.com>
2022-06-19 16:05:33 -0300

    correçãocategorias HOMe inclusao banner nao funcionando ainda

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css' 
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './Components/Categorias'
 import Categorias from './Components/Categorias';
 import './style.css';
+
 const Home = () => {
-    return(
+    return (
         <>
-          <h1 className='titulo'>Categorias</h1>
-          <Categorias />
+            <img src='../../assets/imagembanner/Banner.svg' alt='baner tegloja' />
+            <h1 className='titulo'>Categorias</h1>
+            <Categorias />
         </>
     )
 }
