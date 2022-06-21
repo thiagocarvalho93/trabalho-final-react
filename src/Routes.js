0be5f4a1e2ef267aefc07cee683ecc2aa1331e95
thiagocarvalho93 <thiagopaes1993@gmail.com>
2022-06-21 13:40:04 -0300

    Rota restrita

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom'
 import Home from './pages/Home';
 import Cadastro from './pages/Cadastro';
 import Contato from './pages/Contato';
@@ -9,15 +9,18 @@ import Cart from './pages/Cart';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import PainelProduto from './pages/PainelProduto';
+import { useState } from 'react';
 
 const Rotas = () => {
+    const [user, setUser] = useState(false)
+
     return(
         <BrowserRouter>
              <Navbar/>   
         <Routes>
             <Route path='/cadastro' element={<Cadastro />} />
             <Route path='/contato' element={<Contato />} />
-            <Route path='/carrinho' element={<Cart />} />
+            <Route path='/carrinho' element={user ? <Cart /> : <Navigate to="/login" />} />
             <Route path='/' element={<Home />} />{/*Main page*/}
             <Route path='/produto' element={<Produto />} />
             <Route path='/sobre' element={<Sobre />} />
@@ -28,6 +31,5 @@ const Rotas = () => {
         <Footer/>
     </BrowserRouter>
     )
- 
 }
 export default Rotas;
