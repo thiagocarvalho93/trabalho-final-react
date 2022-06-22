c558a62d9dabb1ff8631249ebebd832f4958ae7d
Vini-Yking <mvdsfg@gmail.com>
2022-06-21 22:12:52 -0300

    ajuste no body

--- src/components/Navbar/Components/PesquisaNome/index.js
+++ src/components/Navbar/Components/PesquisaNome/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 const PesquisaNome = (setProdutosFiltrados)=>{
 
     const searchCategoria = async () => {
--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './style.css'
+
 
 const Sobre = () => {
     return(
